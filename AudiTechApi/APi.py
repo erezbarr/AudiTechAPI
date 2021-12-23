ce20f6251061b6cb0517b43afaf58823e77380a5
erez.barr <erezbarr@gmail.com>
2021-12-23 09:46:24 -0800

    Initial commit

---
+++ AudiTechApi/APi.py
@@ -0,0 +1,50 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+import requests
+import json
+import mysql.connector
+import threading
+from AudiTechApi.github_listener.listener import RunListener as Listener
+
+
+class Pullrequests(Resource):
+    def post(self):
+        if "New Request" == request.headers.get('PullRequest'):
+            response = requests.get('https://api.github.com/repos/erezbarr/auditech/pulls?state=all')
+            json_content = json.loads(response.content)
+            new_pull_request = json_content[0]
+            url = new_pull_request['url']
+            id_number = new_pull_request['id']
+
+            number = new_pull_request['number']
+            state = new_pull_request['state']
+            user = new_pull_request['user']['login']
+            title = new_pull_request['title']
+            created_at = new_pull_request['created_at']
+            mydb = mysql.connector.connect(host="localhost",
+                                           user="pullrequestor",
+                                           password="password",
+                                           database="pullrequests")  # take into account that the DB and table exists
+            mycursor = mydb.cursor()
+            sql = "INSERT INTO requestsdata (Url, Id, Number, State, User, Title, CreationTime) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+            val = (url, id_number, number, state, user, title, created_at)
+            mycursor.execute(sql, val)
+            print("New Pull Request - Server")
+
+
+class myThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        listener_instance = Listener()
+        listener_instance.run_me()
+
+
+app = Flask(__name__)
+api = Api(app)
+api.add_resource(Pullrequests, '/pullrequests')  # '/users' is our entry point
+thread = myThread()
+if __name__ == "__main__":
+    thread.start()
+    app.run()
---
+++ AudiTechApi/github_listener/listener.py
@@ -0,0 +1,54 @@
+import github
+import requests
+import time
+
+class PullNotifs:
+
+    def __init__(self):
+        self.git_client = github.Github("erezbarr", "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")
+        self.pulls = []
+        self.pull_counts = {}
+
+    def get_all_repos(self):
+        self.pulls = []
+        count = 0
+        for i in self.git_client.get_user().get_repos():
+            self.pulls.append(i.get_pulls('all'))
+            self.pull_counts[count] = 0
+            count += 1
+
+    def set_pull_counts(self):
+        count = 0
+        for repo in self.pulls:
+            for j in repo:
+                self.pull_counts[count] = self.pull_counts[count] + 1
+            count += 1
+
+    def check_counts(self):
+        counts_check = {}
+        count = 0
+        for pl in self.git_client.get_user().get_repos():
+            pl_repos = pl.get_pulls('all')
+            counts_check[count] = 0
+            for repo in pl_repos:
+                counts_check[count] = counts_check[count] + 1
+            count += 1
+        return self.pull_counts == counts_check
+
+    def send_message(self):
+        print("new pull request - listener")
+        # Making a POST request
+        r = requests.post('http://127.0.0.1:5000/pullrequests', headers={'PullRequest': 'New Request'})
+
+class RunListener:
+    def run_me(self):
+        labs = PullNotifs()
+        labs.get_all_repos()
+        labs.set_pull_counts()
+        while True:
+            time.sleep(10)
+            if not labs.check_counts():
+                labs.send_message()
+                labs.pull_counts = {}
+                labs.get_all_repos()
+                labs.set_pull_counts()
